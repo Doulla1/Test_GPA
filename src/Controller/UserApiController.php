e93ec1b49052dc1e1494c3f7260ae74394551573
Abdoulaye <dialloabdoul99c@gmail.com>
2024-12-16 10:05:02 +0100

    fix: UserApiController should extend AbstractController

--- src/Controller/UserApiController.php
+++ src/Controller/UserApiController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use App\Dto\UserDto;
-use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Service\UserService;
 use Doctrine\ORM\EntityNotFoundException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/users', name: 'api_users_')]
-class UserApiController
+class UserApiController extends AbstractController
 {
     public function __construct(
         private UserRepository $userRepository,
